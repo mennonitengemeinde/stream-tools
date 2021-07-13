a5d1ed815e20d9222ebed5479c256849fdd43ec2
Daniel Braun <daniel@pomas.ca>
2021-07-13 19:44:43 -0400

    initial design for clock page

---
+++ client-ng/src/app/pages/clock/clock.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.scss'],
+})
+export class ClockComponent implements OnInit {
+  time: Date = new Date();
+
+  constructor() {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  ngOnInit(): void {}
+}
